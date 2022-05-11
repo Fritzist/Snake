ea104ea9c02b4f8f338b85462f6fbd95ab074325
Fritzist <danielhartl856@gmail.com>
2022-05-11 21:31:08 +0200

    added a readme file i

--- app.js
+++ app.js
@@ -1,185 +1,191 @@
 
-        let canvas = document.getElementById("canvas");
-        let ctx = canvas.getContext("2d");
-        let rows = 20;
-        let cols = 20;
-        let snake = [{
-            x: 19, 
-            y:3
-        }];
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let rows = 20;
+let cols = 20;
+let snake = [{
+    x: 19,
+    y:3
+}];
 
-        let food;
-        let cellWidth = canvas.width / cols;
-        let cellHeight = canvas.height / rows;
-        let direction = "LEFT";
-        let foodcollected = false;
+let food;
+let cellWidth = canvas.width / cols;
+let cellHeight = canvas.height / rows;
+let direction = "LEFT";
+let foodcollected = false;
 
-        placeFood();
+placeFood();
 
-        setInterval(gameLoop, 200);
+setInterval(gameLoop, 200);
 
-        document.addEventListener("keydown", keyDown);
-
-    
-        draw();
-
-        function draw() {
-
-            //background
-            ctx.fillStyle = "black";
-            ctx.fillRect(0, 0, canvas.width, canvas.height); // canvas.width / height = automatisch die größe von dem canvas
-            
-            //player / snake
-            ctx.fillStyle = "white";
-            add(100, 150);
-            add(130, 150);
-            add(160, 150);
-            add(190, 150);
-            add(220, 150);
+document.addEventListener("keydown", keyDown);
 
 
-            snake.forEach(part => add(part.x, part.y));
-            
+draw();
 
-            //food
-            ctx.fillStyle = "cyan";
-            add(food.x, food.y);
+function draw() {
 
-            requestAnimationFrame(draw);
+    //background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // canvas.width / height = automatisch die größe von dem canvas
 
-        }
+    //player / snake
+    ctx.fillStyle = "white";
+    add(100, 150);
+    add(130, 150);
+    add(160, 150);
+    add(190, 150);
+    add(220, 150);
 
-        function gameEnd() {
 
-            //gegen sich selber
-            let firstPart =snake[0];
-            let otherParts = snake.slice(1);
-            let duplicatePart = otherParts.find(part => part.x == firstPart.x && part.y == firstPart.y);
+    snake.forEach(part => add(part.x, part.y));
 
-            //gegen die Wand
-            if (snake[0].x < 0 || 
-                snake[0].x > cols -1 ||
-                snake[0].y < 0 ||
-                snake[0].y > rows -1 ||
-                duplicatePart
-        ) {
 
-            placeFood();
-            
-            snake = [{
-                x: 19, 
-                y:3
-            }];
+    //food
+    ctx.fillStyle = "cyan";
+    add(food.x, food.y);
 
-            direction = "LEFT";
+    requestAnimationFrame(draw);
 
-        }
+}
+
+function gameEnd() {
+
+    //gegen sich selber
+    let firstPart =snake[0];
+    let otherParts = snake.slice(1);
+    let duplicatePart = otherParts.find(part => part.x == firstPart.x && part.y == firstPart.y);
+
+    //gegen die Wand
+    if (snake[0].x < 0 ||
+        snake[0].x > cols -1 ||
+        snake[0].y < 0 ||
+        snake[0].y > rows -1 ||
+        duplicatePart
+
+
+) {
+
+    placeFood();
+
+    snake = [{
+        x: 19,
+        y:3
+    }];
+
+    direction = "LEFT";
+
+    winningMessageElement.classList.add('show')
+    winningMessageTextElement.textContent = "You Lose"
+    restartButton.addEventListener('click', reloadPage)
+
+}
+}
+
+function placeFood() {
+
+    let randomX = Math.floor(Math.random() * cols);
+    let randomY = Math.floor(Math.random() * rows);
+
+    food = {x: randomX, y: randomY};
+
+}
+
+function add(x, y) {
+
+    ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth - 1, cellHeight - 1);
+
+}
+
+function shiftSnake() {
+
+    for (let i = snake.length - 1; i > 0; i--) {
+        const part = snake[i];
+        const lastpart = snake[i - 1];
+        part.x = lastpart.x;
+        part.y = lastpart.y;
+
+    }
+}
+
+function gameLoop() {
+    gameEnd();
+
+    if (foodcollected) {
+        snake = [{
+            x: snake[0].x,
+            y: snake[0].y
+        }, ...snake];
+
+        foodcollected = false;
+
     }
 
-        function placeFood() {
+    shiftSnake();
 
-            let randomX = Math.floor(Math.random() * cols);
-            let randomY = Math.floor(Math.random() * rows); 
+    if (direction == "LEFT") {
+        snake[0].x--;
+    }
 
-            food = {x: randomX, y: randomY};
+    if (direction == "RIGHT") {
+        snake[0].x++;
+    }
 
-        }
+    if (direction == "UP") {
+        snake[0].y--;
+    }
 
-        function add(x, y) {
+    if (direction == "DOWN") {
+        snake[0].y++;
+    }
 
-            ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth - 1, cellHeight - 1);
+    if (snake[0].x == food.x &&
+        snake[0].y == food.y) {
 
-        }
+            foodcollected = true;
 
-        function shiftSnake() {
+            placeFood(); // place the food again
 
-            for (let i = snake.length - 1; i > 0; i--) {
-                const part = snake[i];
-                const lastpart = snake[i - 1];
-                part.x = lastpart.x;
-                part.y = lastpart.y;
-                
-            }
-        }
+    }
+}
 
-        function gameLoop() {
-            gameEnd();
+const directions = {
+    37: "LEFT",
+    65: "LEFT",
+    38: "UP",
+    87: "UP",
+    39: "RIGHT",
+    68: "RIGHT",
+    40: "DOWN",
+    83: "DOWN",
 
-            if (foodcollected) {
-                snake = [{
-                    x: snake[0].x, 
-                    y: snake[0].y
-                }, ...snake];
+    }
 
-                foodcollected = false;
+    function keyDown(e) {
+    const tmpDirection = directions[e.keyCode];
+    if(tmpDirection) direction = tmpDirection;
+    }
 
-            }
+    function pressedUp() {
 
-            shiftSnake();
+        direction = "UP"
 
-            if (direction == "LEFT") {
-                snake[0].x--;
-            }
-            
-            if (direction == "RIGHT") {
-                snake[0].x++;
-            }
+    }
 
-            if (direction == "UP") {
-                snake[0].y--;
-            }
+    function pressedDown() {
 
-            if (direction == "DOWN") {
-                snake[0].y++;
-            }
+        direction = "DOWN"
 
-            if (snake[0].x == food.x &&
-                snake[0].y == food.y) {
+    }
 
-                    foodcollected = true;    
+    function pressedLeft() {
 
-                    placeFood(); // place the food again
+        direction = "LEFT"
 
-            }
-        }
+    }
 
-        const directions = {
-            37: "LEFT",
-            65: "LEFT",
-            38: "UP",
-            87: "UP",
-            39: "RIGHT",
-            68: "RIGHT",
-            40: "DOWN",
-            83: "DOWN",
+    function pressedRight() {
 
-            }
+        direction = "RIGHT"
 
-            function keyDown(e) {
-            const tmpDirection = directions[e.keyCode];
-            if(tmpDirection) direction = tmpDirection;
-            }
-            
-            function pressedUp() {
-
-                direction = "UP"
-
-            }
-
-            function pressedDown() {
-
-                direction = "DOWN"
-
-            }
-
-            function pressedLeft() {
-
-                direction = "LEFT"
-
-            }
-
-            function pressedRight() {
-
-                direction = "RIGHT"
-
-            }
+    }
